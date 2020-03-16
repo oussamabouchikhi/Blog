2a6e79bb3eca81f9e6e1f2b83a45ca18b81ec568
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-03-16 17:53:49 +0100

    soft delete posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -89,8 +89,12 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        session()->flash('success', 'post moved to trash successfully');
+
+        return redirect(route('posts.index'));
     }
 }
